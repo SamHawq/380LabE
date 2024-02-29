81f445f744cd113522dfcb1691a15d6d7cef5a21
Adeeb Hossain <adeebhossain2004@gmail.com>
2024-02-29 14:23:24 -0700

    added directories

---
+++ edu/ucalgary/oop/Task.java
@@ -0,0 +1,49 @@
+package edu.ucalgary.oop;
+
+import java.util.Objects;
+
+public class Task {
+    private int id;
+    private String title;
+    private boolean isCompleted;
+
+    // Constructor
+    public Task(int id, String title, boolean isCompleted) {
+        this.id = id;
+        this.title = title;
+        this.isCompleted = isCompleted;
+    }
+
+    // Getter methods for id, title, and isCompleted
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    // Implementing the copy method for deep copying task objects
+    public Task copy() {
+        return new Task(this.id, this.title, this.isCompleted);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Task task = (Task) obj;
+        return Objects.equals(id, task.id) &&
+               Objects.equals(title, task.title) &&
+               isCompleted == task.isCompleted;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, isCompleted);
+    }
+}
--- edu/ucalgary/oop/ToDoListTest.java
+++ edu/ucalgary/oop/ToDoListTest.java
@@ -3,6 +3,7 @@ package edu.ucalgary.oop;
 
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 import java.util.List;
 
