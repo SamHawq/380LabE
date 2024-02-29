5924aa171bffe00d511140bf38886d5b04e735b6
malihachowdhury6 <144377170+malihachowdhury6@users.noreply.github.com>
2024-02-29 14:33:28 -0600

    added IToDoList

--- edu/ucalgary/oop/IToDoList.java
+++ edu/ucalgary/oop/IToDoList.java
@@ -1,3 +1,20 @@
 public class IToDoList {
+    public interface IToDoList {
+        // Adds a new task to the list
+        void addTask(Task task);
+    
+        // Deletes a task from the list by its ID
+        void deleteTask(String taskId);
+    
+        // Marks a specified task as completed by its ID
+        void markTaskCompleted(String taskId);
+    
+        // Undoes the last operation (addition, deletion, completion, or edit)
+        void undo();
+    
+        // Edits an existing task's title or completion status
+        void editTask(String taskId, String newTitle, boolean isCompleted);
+    }
+    
     
 }
